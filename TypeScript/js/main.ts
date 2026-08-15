1618b995712ead94cea1c53ce59de4f115a8c211
Fidan Umudova <fidanumudova05@gmail.com>
2026-08-15 23:08:25 +0400

    Update main.ts

--- TypeScript/js/main.ts
+++ TypeScript/js/main.ts
@@ -1,1 +1,39 @@
+interface Student{
+  firstName: string;
+  lastName: string;
+  age: number;
+  locatiob: string;
+}
+const student1: Student = {
+  firstName: "Alex",
+  lastName: "Rivera",
+  age: 20,
+  location: "Baku"
+};
 
+const student2: Student = {
+  firstName: "Elena",
+  lastName: "Rostova",
+  age: 22,
+  location: "Tallinn"
+};
+
+const studentList: Array<student> = [Student1,Student2];
+const table: HTMLTableElement = document.createElement("table");
+const tbody: HTMLTableSelectionElement = document.createElement("tbody");
+
+studentsList.forEach((student: Student): void => {
+  const row: HTMLTableRowElement = document.createElement("tr");
+  const firstNameCell: HTMLTableCellElement = document.createElement("td");
+  const locationCell: HTMLTableCellElement = document.createElement("td");
+
+  firstNameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
