ee934f14157e06aaf692bf0de8cc4b3cb3f1f54d
Fidan Umudova <fidanumudova05@gmail.com>
2026-08-15 23:24:25 +0400

    Update main.ts

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-// Task 1
+// Task 1: Teacher interfeysi
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -8,12 +8,12 @@ interface Teacher {
   [propName: string]: any;
 }
 
-// Task 2
+// Task 2: Directors interfeysi
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Task 3
+// Task 3: Funksiya interfeysi və funksiya
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
@@ -21,6 +21,34 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+// Task 4: StudentClass interfeysləri və klassı
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
 
 // Testlər
 const teacher3: Teacher = {
@@ -39,6 +67,10 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+const student = new StudentClass("Aysel", "Mammadova");
+
 console.log(teacher3);
 console.log(director1);
 console.log(printTeacher("John", "Doe"));
+console.log(student.displayName());
+console.log(student.workOnHomework());
